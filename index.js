49a3fcc0691ba8afeed11bb4687c4e98f22db6ec
DawidB7 <podlasieauuau@gmail.com>
2026-08-21 01:43:58 +0200

    test2

--- index.js
+++ index.js
@@ -662,45 +662,6 @@ function speak (phrase) {
   window.speechSynthesis.speak(new window.SpeechSynthesisUtterance(phrase))
 }
 
-/**
- * Start an annoying theramin that changes pitch and volume depending on
- * the mouse position. Uses a Web Audio oscillator. Reauires user-initiated
- * event.
- * Based on https://github.com/feross/TheAnnoyingSite.com/pull/2
- */
-function startTheramin () {
-  const audioContext = new AudioContext()
-  const oscillatorNode = audioContext.createOscillator()
-  const gainNode = audioContext.createGain()
-
-  const pitchBase = 50
-  const pitchRange = 4000
-
-  const wave = audioContext.createPeriodicWave(
-    Array(10).fill(0).map((v, i) => Math.cos(i)),
-    Array(10).fill(0).map((v, i) => Math.sin(i))
-  )
-
-  oscillatorNode.setPeriodicWave(wave)
-
-  oscillatorNode.connect(gainNode)
-  gainNode.connect(audioContext.destination)
-
-  oscillatorNode.start(0)
-
-  const oscillator = ({ pitch, volume }) => {
-    oscillatorNode.frequency.value = pitchBase + pitch * pitchRange
-    gainNode.gain.value = volume * 3
-  }
-
-  document.body.addEventListener('mousemove', event => {
-    const { clientX, clientY } = event
-    const { clientWidth, clientHeight } = document.body
-    const pitch = (clientX - clientWidth / 2) / clientWidth
-    const volume = (clientY - clientHeight / 2) / clientHeight
-    oscillator({ pitch, volume })
-  })
-}
 
 /**
  * Attempt to read the user's clipboard.
